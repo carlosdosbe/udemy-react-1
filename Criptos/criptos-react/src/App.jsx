84fe42c60c61bda9880ea865604563e93839906b
Carlos Daniel OB <carlosdosbe@gmail.com>
2023-05-03 17:24:00 -0600

    Clase 137

---
+++ Criptos/criptos-react/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import styled from "@emotion/styled";
+
+const Heading = styled.h1`
+  color: #41403e;
+`;
+
+function App() {
+  return (
+    <>
+      <Heading>Hola</Heading>
+    </>
+  );
+}
+
+export default App;
