fbca3466f620727c1113de5aa8d9bbe9a0ff6302
Carlos Daniel OB <carlosdosbe@gmail.com>
2023-05-09 12:24:59 -0600

    Consultando api criptos

--- Criptos/criptos-react/src/components/Formulario.jsx
+++ Criptos/criptos-react/src/components/Formulario.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import styled from "@emotion/styled";
+import useSelectMonedas from "../hooks/useSelectMonedas";
+import { monedas } from "../data/monedas";
 
 const InputSubmit = styled.input`
   background-color: #93b3ec;
@@ -12,6 +14,7 @@ const InputSubmit = styled.input`
   border-radius: 10px;
   font-size: 20px;
   transition: all 0.3s ease;
+  margin-top: 25px;
 
   &:hover {
     background-color: #4e78e2;
@@ -20,8 +23,34 @@ const InputSubmit = styled.input`
 `;
 
 const Formulario = () => {
+  const [criptos, setCriptos] = useState([]);
+  const [moneda, SelectMonedas] = useSelectMonedas("Elige tu moneda", monedas);
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url =
+        "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=15&tsym=USD";
+
+      const respuesta = await fetch(url);
+      const resultado = await respuesta.json();
+
+      const arrayCriptos = resultado.Data.map((cripto) => {
+        const objeto = {
+          id: cripto.CoinInfo.Name,
+          nombre: cripto.CoinInfo.FullName,
+        };
+        return objeto;
+      });
+      setCriptos(arrayCriptos);
+    };
+
+    consultarAPI();
+  }, []);
+
   return (
     <form>
+      <SelectMonedas />
+      {moneda}
       <InputSubmit type="submit" value="Consultar" />
     </form>
   );
---
+++ Criptos/criptos-react/src/data/monedas.js
@@ -0,0 +1,7 @@
+const monedas = [
+  { id: "USD", nombre: "Dolar US" },
+  { id: "MXN", nombre: "Peso Mexicano" },
+  { id: "EUR", nombre: "Euro" },
+];
+
+export { monedas };
